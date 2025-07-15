3999a9aa92f3fc2796cece88279da37cbce65c72
S Dhanush <dhanushsreelakshmi@gmail.con>
2025-07-15 15:27:01 +0530

    Updated image code

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -8,9 +8,12 @@ function PostCard({$id, title, featuredImage}) {
     <Link to={`/post/${$id}`}>
         <div className='w-full bg-gray-100 rounded-xl p-4'>
             <div className='w-full justify-center mb-4'>
+              {featuredImage ? (
                 <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
                 className='rounded-xl' />
-
+              ) : (
+                <p className='text-gray-500'>No image available</p>
+              )}
             </div>
             <h2
             className='text-xl font-bold'
